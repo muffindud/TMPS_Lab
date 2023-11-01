43aeffbebd21db949fecba041cefa08e2b5e79e6
muffindud <ccatlabuga2002@gmail.com>
2023-11-01 19:17:13 +0200

    add blank inventry management class

---
+++ src/main/kotlin/patterns/structurals/bridge/InventoryManager.kt
@@ -0,0 +1,26 @@
+package patterns.structurals.bridge
+
+import game.item.IItem
+import game.item.equipment.AEquipment
+
+object InventoryManager {
+    fun dropItem(item: IItem) {
+        TODO()
+    }
+
+    fun pickItem(item: IItem) {
+        TODO()
+    }
+
+    fun equip(item: AEquipment) {
+        TODO()
+    }
+
+    fun unequip(item: AEquipment) {
+        TODO()
+    }
+
+    fun deleteItem(item: IItem) {
+        TODO()
+    }
+}
