1be1212c69025677b817353414f646c0ca8ba3e6
muffindud <ccatlabuga2002@gmail.com>
2023-11-19 16:25:23 +0200

    player params instantiation

--- src/main/kotlin/game/player/Player.kt
+++ src/main/kotlin/game/player/Player.kt
@@ -5,31 +5,41 @@ import game.playerProfile.PlayerProfile
 import game.tank.ITank
 import game.crew.Crew
 import game.equipment.IEquipment
+import serverAPI.Server
 
-abstract class Player(
-    var profile: PlayerProfile
+class Player(
+    profile: PlayerProfile,
+    server: Server
 ) {
-    class PlayerGarage {
+    class PlayerGarage (var playerData: String) {
         var tanks: List<ITank> = listOf()
 
         init {
-
+            // Get the tanks from playerData and add them to the list
         }
     }
 
-    class PlayerBarracks {
+    class PlayerBarracks (var playerData: String) {
         var crews: List<Crew> = listOf()
 
         init {
-
+            // Get the crews from playerData and add them to the list
         }
     }
 
-    class PlayerEquipment {
+    class PlayerEquipment (var playerData: String) {
         var equipments: HashMap<IEquipment, Int> = hashMapOf()
 
         init {
-
+            // Get the equipments from playerData and add them to the list
         }
+    }
+
+    init {
+        val playerData = server.getPlayerData(profile.playerId)
+
+        val garage = PlayerGarage(playerData)
+        val barracks = PlayerBarracks(playerData)
+        val equipment = PlayerEquipment(playerData)
     }
 }
