38e1ac15503993b29f60f5caaab4b9eefce22d4e
muffindud <ccatlabuga2002@gmail.com>
2023-11-19 16:06:55 +0200

    start composite implementation

--- src/main/kotlin/game/player/Player.kt
+++ src/main/kotlin/game/player/Player.kt
@@ -2,8 +2,34 @@ package game.player
 
 import game.playerProfile.PlayerProfile
 
+import game.tank.ITank
+import game.crew.Crew
+import game.equipment.IEquipment
+
 abstract class Player(
     var profile: PlayerProfile
 ) {
+    class PlayerGarage {
+        var tanks: List<ITank> = listOf()
 
+        init {
+
+        }
+    }
+
+    class PlayerBarracks {
+        var crews: List<Crew> = listOf()
+
+        init {
+
+        }
+    }
+
+    class PlayerEquipment {
+        var equipments: HashMap<IEquipment, Int> = hashMapOf()
+
+        init {
+
+        }
+    }
 }
