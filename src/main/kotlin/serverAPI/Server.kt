1a6992b3367d302ffe3f71f45e013d565409f312
muffindud <ccatlabuga2002@gmail.com>
2023-11-19 16:24:12 +0200

    add server api class

---
+++ src/main/kotlin/serverAPI/Server.kt
@@ -0,0 +1,12 @@
+package serverAPI
+
+class Server {
+    val hostname: String = ""
+
+    fun getPlayerData (playerId: String): String {
+        // Make request to server
+        // Return player data
+
+        return ""
+    }
+}
