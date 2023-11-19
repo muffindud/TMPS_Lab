680374d2d76c48317558369e5a575c9511e8618f
muffindud <ccatlabuga2002@gmail.com>
2023-11-19 15:50:19 +0200

    implement player profiles

---
+++ src/main/kotlin/game/player/Player.kt
@@ -0,0 +1,9 @@
+package game.player
+
+import game.playerProfile.PlayerProfile
+
+abstract class Player(
+    var profile: PlayerProfile
+) {
+
+}
---
+++ src/main/kotlin/game/playerProfile/PlayerProfile.kt
@@ -0,0 +1,43 @@
+package game.playerProfile
+
+import java.io.File
+import java.io.FileWriter
+import java.util.Scanner
+
+class PlayerProfile {
+    var playerId: String = ""
+    var playerName: String = ""
+    var playerEmail: String = ""
+
+    constructor(
+        playerId: String,
+        playerName: String,
+        playerEmail: String
+    ) {
+        this.playerId = playerId
+        this.playerName = playerName
+        this.playerEmail = playerEmail
+    }
+
+    constructor(
+        playerFileName: String
+    ) {
+        val playerFileReader = Scanner(File(playerFileName))
+
+        this.playerId = playerFileReader.nextLine()
+        this.playerName = playerFileReader.nextLine()
+        this.playerEmail = playerFileReader.nextLine()
+    }
+
+    fun saveProfile () {
+        val playerFile = File("./players/" + this.playerName + ".txt")
+        playerFile.createNewFile()
+
+        val playerFileWriter = FileWriter("./players/" + this.playerName + ".txt")
+        playerFileWriter.write(this.playerId + "\n" + this.playerName + "\n" + this.playerEmail)
+    }
+
+    fun changeName (newName: String) {
+        playerName = newName
+    }
+}
